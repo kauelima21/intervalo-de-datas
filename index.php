d69c0d9d5afeb499ea4636a99b05b7d5da8ab3fb
kaue lima <kaueslim@outlook.com>
2021-07-01 19:37:53 -0300

    primeiro passo dado

--- index.php
+++ index.php
@@ -5,15 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Intervalo de datas</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <form action="">
+    <h1>Calcular o Intervalo das Datas</h1>
+    <form method="post" action="calcInterval.php">
         <fieldset>
             <legend>Selecione as datas</legend>
             <label for="date1">Data de início</label>
             <input type="date" name="date_start" id="date1">
             <label for="date2">Data final</label>
             <input type="date" name="date_end" id="date2">
+            <button>Calcular</button>
         </fieldset>
     </form>
 </body>
