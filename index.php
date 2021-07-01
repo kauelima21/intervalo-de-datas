ec9f239ebaf5062705c0f2b87adbc426fcfa525e
kaue lima <kaueslim@outlook.com>
2021-07-01 19:19:23 -0300

    dando inicio ao projeto

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Intervalo de datas</title>
+</head>
+<body>
+    <form action="">
+        <fieldset>
+            <legend>Selecione as datas</legend>
+            <label for="date1">Data de início</label>
+            <input type="date" name="date_start" id="date1">
+            <label for="date2">Data final</label>
+            <input type="date" name="date_end" id="date2">
+        </fieldset>
+    </form>
+</body>
+</html>
