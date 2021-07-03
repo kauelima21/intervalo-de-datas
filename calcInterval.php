e174737a370fd55047631304bad217e7908107e1
kaue lima <kaueslim@outlook.com>
2021-07-03 14:56:21 -0300

    back end pronto

--- calcInterval.php
+++ calcInterval.php
@@ -1,6 +1,16 @@
 <?php
 
-$date_start = addslashes($_POST["date_start"]);
-$date_end = addslashes($_POST["date_end"]);
+$date_start = new DateTime( $_POST["date_start"]);
+$date_end = new DateTime( $_POST["date_end"]);
 
+$diff = $date_start->diff($date_end);
 
+session_start();
+
+$_SESSION['message'] = <<<MSG
+    <div class='message'>
+        <p>A diferença é de {$diff->days} dias.<p>
+    </div>
+MSG;
+
+header("location: index.php");
